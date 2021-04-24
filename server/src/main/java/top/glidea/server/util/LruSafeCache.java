ff1283c50f32b9372c148113149a9f17a3deedc8
glidea <740696441@qq.com>
2021-04-24 16:24:35 +0800

    u4

--- server/src/main/java/top/glidea/server/util/LruSafeCache.java
+++ server/src/main/java/top/glidea/server/util/LruSafeCache.java
@@ -1,143 +1,238 @@
 package top.glidea.server.util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * 线程安全的LRU Cache
- * TODO 待优化
  */
 public class LruSafeCache<K, V> {
-    private Map<K, Node<K, V>> map = new HashMap<>();
-    private DoubleLinkedList<K, V> list = new DoubleLinkedList<>();
-    /**
-     * 缓存最大容量
-     */
-    private final int DEFAULT_MAX_CAP = 1024;
-    private int maxCap = DEFAULT_MAX_CAP;
-    /**
-     * 读写锁
-     */
-    private ReadWriteLock rw = new ReentrantReadWriteLock();
-    private Lock readLock = rw.readLock();
-    private Lock writeLock = rw.writeLock();
+    private final Map<K, Node<K, V>> map;
+    private final ConcurrentOrderList<K, V> list;
+    private final int maxCap;
+
+    private static final int DEFAULT_MAX_CAP = 1024;
+    private static final int DEFAULT_CONCURRENCY_LEVEL = 4;
 
     public LruSafeCache() {
+        this(DEFAULT_MAX_CAP, DEFAULT_CONCURRENCY_LEVEL);
     }
 
     public LruSafeCache(int maxCap) {
-        if (maxCap <= 0) {
-            throw new IllegalArgumentException("Cache 的容量必须为正整数！");
-        }
+        this(maxCap, DEFAULT_CONCURRENCY_LEVEL);
+    }
+
+    /**
+     * concurrentLevel 代表ConcurrentOrderList#Segment 的个数
+     * 值越大，LruSafeCache读读，读写并发度越高（写写仍互斥，但因为缓存本就是读多写少才用，所以即便支持并发修改，性能提高也很有限）
+     * 但代价是实际缓存淘汰策略会逐步背离 LRU
+     */
+    public LruSafeCache(int maxCap, int concurrentLevel) {
         this.maxCap = maxCap;
+        map = new HashMap<>();
+        list = new ConcurrentOrderList<>(concurrentLevel);
     }
 
+    /**
+     * no lock，弱一致性
+     */
     public V get(K key) {
-        readLock.lock();
-        try {
-            if (!map.containsKey(key)) {
-                return null;
-            }
-
-            Node<K, V> node = map.get(key);
-            list.moveToTail(node);
-            return node.value;
-
-        } finally {
-            readLock.unlock();
+        Node<K, V> node = map.get(key);
+        if (node == null) {
+            return null;
         }
+
+        list.moveToLast(node);
+        return node.value;
     }
 
-    public void put(K key, V value) {
-        writeLock.lock();
-        try {
-            if (map.containsKey(key)) {
-                // put 为修改操作
-                Node<K, V> node = map.get(key);
-                node.value = value;
-                map.put(key, node);
+    public synchronized void put(K key, V value) {
+        if (map.containsKey(key)) {
+            // put 为修改操作
+            Node<K, V> node = map.get(key);
+            node.value = value;
+            map.put(key, node);
 
-                list.moveToTail(node);
-                return;
-            }
+            list.moveToLast(node);
+            return;
+        }
 
-            // put 为新增操作
-            if (map.size() == maxCap) {
-                // 容量不足，淘汰上次使用时间离现在最久的 key-value
-                Node<K, V> delNode = list.removeFirst();
+        // put 为新增操作
+        if (map.size() == maxCap) {
+            // 容量不足，淘汰上次使用时间离现在最久的 key-value
+            List<Node<K, V>> delNodes = list.removeAllFirst();
+            for (Node<K, V> delNode : delNodes) {
                 map.remove(delNode.key);
             }
-            Node<K, V> node = new Node<>(key, value);
-            map.put(key, node);
-            list.addLast(node);
-
-        } finally {
-            writeLock.unlock();
         }
+        Node<K, V> node = new Node<>(key, value);
+        map.put(key, node);
+        list.addLast(node);
     }
 
-    public void remove(K key) {
-        writeLock.lock();
-        try {
-            Node<K, V> delNode = map.remove(key);
-            list.remove(delNode);
-        } finally {
-            writeLock.unlock();
-        }
+    public synchronized void remove(K key) {
+        Node<K, V> delNode = map.remove(key);
+        list.remove(delNode);
     }
 
 
-    private static class Node<K, V> {
-        K key;
-        V value;
-        Node<K, V> prev, next;
+    /* ---------------- ConcurrentOrderList -------------- */
+    /**
+     * 维护使用顺序的双向链表
+     * 支持并发修改
+     */
+    private static final class ConcurrentOrderList<K, V> {
+        private final Segment<K, V>[] segments;
 
-        public Node(K key, V value) {
+        @SuppressWarnings("unchecked")
+        ConcurrentOrderList(int concurrentLevel) {
+            int n = overToPowerOf2(concurrentLevel);
+            segments = (Segment<K, V>[]) new Segment[n];
+            for (int i = 0; i < segments.length; i++) {
+                segments[i] = new Segment<>();
+            }
+        }
+
+        /**
+         * 将node 添加到链表尾部
+         */
+        void addLast(Node<K, V> node) {
+            int idx = locate(node.key.hashCode());
+            segments[idx].addLast(node);
+        }
+
+        /**
+         * 移除node
+         */
+        void remove(Node<K, V> node) {
+            int idx = locate(node.key.hashCode());
+            segments[idx].remove(node);
+        }
+
+        /**
+         * 移除所有Segment 链表的头部
+         */
+        List<Node<K, V>> removeAllFirst() {
+            List<Node<K, V>> firsts = new ArrayList<>();
+            for (Segment<K, V> segment : segments) {
+                Node<K, V> first = segment.removeFirst();
+                if (first != null) {
+                    firsts.add(first);
+                }
+            }
+            return firsts;
+        }
+
+        /**
+         * 移动node 到末尾
+         */
+        void moveToLast(Node<K, V> node) {
+            int idx = locate(node.key.hashCode());
+            segments[idx].moveToLast(node);
+        }
+
+        /**
+         * hash定位子链表
+         */
+        private int locate(int hashCode) {
+            int spread = (hashCode ^ (hashCode >>> 16)) & 0x7fffffff;
+            return spread & (segments.length - 1);
+        }
+
+        /**
+         * 将n -> 2次幂（大于n）
+         */
+        private int overToPowerOf2(int n) {
+            int r = n - 1;
+            r |= r >>> 1;
+            r |= r >>> 2;
+            r |= r >>> 4;
+            r |= r >>> 8;
+            r |= r >>> 16;
+            return (r < 0) ? 1 : r + 1;
+        }
+
+
+        /* ---------------- Segment -------------- */
+        /**
+         * 子链表（Thread Safe）
+         */
+        final static class Segment<K, V> {
+            private Node<K, V> dummyHead, dummyTail;
+
+            public Segment() {
+                dummyHead = new Node<>();
+                dummyTail = new Node<>();
+                dummyHead.next = dummyTail;
+                dummyTail.prev = dummyHead;
+            }
+
+            /**
+             * 将node 添加到链表尾部
+             */
+            public synchronized void addLast(Node<K, V> node) {
+                node.prev = dummyTail.prev;
+                node.next = dummyTail;
+                dummyTail.prev.next = node;
+                dummyTail.prev = node;
+            }
+
+            /**
+             * 移除node
+             */
+            public synchronized void remove(Node<K, V> node) {
+                if (node.prev == null || node.next == null) {
+                    return;
+                }
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+                node.prev = null;
+                node.next = null;
+            }
+
+            /**
+             * 移除链表头部
+             */
+            public synchronized Node<K, V> removeFirst() {
+                Node<K, V> first = dummyHead.next;
+                if (first != null) {
+                    remove(first);
+                }
+                return first;
+            }
+
+            /**
+             * 移动node 到末尾
+             */
+            public synchronized void moveToLast(Node<K, V> node) {
+                if (node.prev == null || node.next == null) {
+                    return;
+                }
+                remove(node);
+                addLast(node);
+            }
+        }
+    }
+
+
+    /* ---------------- Node -------------- */
+    /**
+     * map的value，list的节点
+     */
+    private static final class Node<K, V> {
+        final K key;
+        volatile V value;
+        volatile Node<K, V> prev, next;
+
+        Node(K key, V value) {
             this.key = key;
             this.value = value;
         }
 
-        public Node() {
-        }
-    }
-
-    /**
-     * TODO 待优化
-     */
-    private static class DoubleLinkedList<K, V> {
-        private Node<K, V> dumyHead, dumyTail;
-
-        public DoubleLinkedList() {
-            dumyHead = new Node<>();
-            dumyTail = new Node<>();
-            dumyHead.next = dumyTail;
-            dumyTail.prev = dumyHead;
-        }
-
-        public synchronized void addLast(Node<K, V> node) {
-            node.prev = dumyTail.prev;
-            node.next = dumyTail;
-            dumyTail.prev.next = node;
-            dumyTail.prev = node;
-        }
-
-        public synchronized void remove(Node<K, V> node) {
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
-        }
-
-        public synchronized Node<K, V> removeFirst() {
-            Node<K, V> first = dumyHead.next;
-            remove(first);
-            return first;
-        }
-
-        public synchronized void moveToTail(Node<K, V> node) {
-            remove(node);
-            addLast(node);
+        Node() {
+            this.key = null;
         }
     }
 }
--- server/src/test/java/Test.java
+++ server/src/test/java/Test.java
@@ -2,26 +2,16 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import lombok.extern.slf4j.Slf4j;
 import top.glidea.common.message.login.LoginRequest;
 import top.glidea.common.util.Serializer;
+import top.glidea.server.util.LruSafeCache;
 
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.LockSupport;
 
 @Slf4j
 public class Test {
-    public static void main(String[] args) {
-        LoginRequest message = new LoginRequest("laowang", "123");
+    public static void main(String[] args) throws InterruptedException {
 
-        byte[] bytes = Serializer.Protobuf.serialize(message, LoginRequest.class);
-        byte[] bytes1 = Serializer.Json.toJsonString(message).getBytes();
-
-        for (byte aByte : bytes) {
-            System.out.print((char)aByte);
-        }
-        System.out.println();
-        for (byte aByte : bytes1) {
-            System.out.print((char)aByte);
-        }
-        System.out.println();
-        System.out.println(Arrays.toString(bytes));
-        System.out.println(Arrays.toString(bytes1));
     }
 }
